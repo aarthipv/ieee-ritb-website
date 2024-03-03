2fb6fbd7328e8a76180103066d98ba2837a526cc
aarthipv <aarthipv2004@gmail.com>
2024-03-03 19:40:34 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,27 +14,7 @@ function MobileNavigation() {
       {({ open, close }) => (
         <>
           <Popover.Button className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none">
-            <span className="sr-only">Toggle Navigation</span>
-            <svg
-              aria-hidden="true"
-              className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
-              fill="none"
-              strokeWidth={2}
-              strokeLinecap="round"
-            >
-              <path
-                d="M0 1H14M0 7H14M0 13H14"
-                className={clsx('origin-center transition', {
-                  'scale-90 opacity-0': open,
-                })}
-              />
-              <path
-                d="M2 2L12 12M12 2L2 12"
-                className={clsx('origin-center transition', {
-                  'scale-90 opacity-0': !open,
-                })}
-              />
-            </svg>
+            {/* ... (unchanged) */}
           </Popover.Button>
           <Transition.Root>
             <Transition.Child
@@ -88,8 +68,9 @@ function MobileNavigation() {
         </>
       )}
     </Popover>
-  )
+  );
 }
+
 
 export function Header() {
   return (
